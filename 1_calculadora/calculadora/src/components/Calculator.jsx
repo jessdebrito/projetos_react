f79e21919ee810c5fef4d434526ab37381c67991
jessdebrito <debrito.herself@gmail.com>
2024-09-14 16:22:43 -0300

    buttons calculator

---
+++ 1_calculadora/calculadora/src/components/Calculator.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+import "./Calculator.css";
+
+const Calculator = () => {
+  const keypadNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const operations = ["+", "-", "*", "/"];
+
+  return (
+    <div className="calculator">
+      <div className="complete-operation">1 + 1 = 2</div>
+      <div className="display">2</div>
+
+      <div className="buttons">
+        <button>AC</button>
+        {keypadNumbers.map((num) => (
+          <button key={num}>{num}</button>
+        ))}
+        {operations.map((operation) => (
+          <button key={operation}>{operation}</button>
+        ))}
+        <button>=</button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Calculator;
---
+++ README.md
@@ -0,0 +1,2 @@
+
+Repositório para reunir exercicios em React e projetos voltados para estudos de lógica 1.
