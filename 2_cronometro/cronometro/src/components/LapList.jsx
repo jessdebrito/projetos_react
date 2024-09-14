2731adcf5155a41f357a9bff41e6e2d748132082
jessdebrito <debrito.herself@gmail.com>
2024-09-14 17:16:02 -0300

    estrutura do cronometro

---
+++ 2_cronometro/cronometro/src/components/LapList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LapList = () => {
+  return (
+    <div>
+      LapList
+    </div>
+  )
+}
+
+export default LapList
---
+++ 2_cronometro/cronometro/src/components/TimerControls.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TimerControls = () => {
+  return (
+    <div>
+      TimerControls
+    </div>
+  )
+}
+
+export default TimerControls
