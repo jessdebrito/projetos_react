18ef457af3c815fb849f10850aea5e563dec98e2
Jéssica de Brito <debrito.herself@gmail.com>
2025-01-31 23:59:00 -0300

    React + TypeScript

---
+++ 20_Ty_multistep_form/src/App.tsx
@@ -0,0 +1,93 @@
+// Componentes
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { FiSend } from "react-icons/fi";
+
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+import Steps from "./components/Steps";
+
+// import { useState } from "react";
+import "./App.css";
+
+// Hooks
+import { useState } from "react";
+import { useForm } from "./hooks/useForm";
+
+type FormFields = {
+  name: string;
+  email: string;
+  review: string;
+  comment: string;
+};
+const formTemplate: FormFields = {
+  name: "",
+  email: "",
+  review: "",
+  comment: "",
+};
+
+function App() {
+  const [data, setData] = useState(formTemplate);
+
+  const updateFieldHandler = (key: string, value: string) => {
+    setData((prev) => {
+      return { ...prev, [key]: value };
+    });
+  };
+
+  const formComponents = [
+    <UserForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <ReviewForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <Thanks data={data} />,
+  ];
+
+  const { currentStep, currentComponent, changeStep, isLastStep } =
+    useForm(formComponents);
+
+  return (
+    <>
+      <div className="app">
+        <div className="header">
+          <h2>Deixe sua avaliação</h2>
+          <p>
+            Ficamos felizes com a sua compra, utilize o formulário abaixo para
+            avaliar o produto
+          </p>
+        </div>
+
+        <div className="form-container">
+          <Steps currentStep={currentStep} />
+          <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+            <div className="inputs-container">{currentComponent}</div>
+            <div className="actions">
+              <button>
+                <GrFormPrevious />
+                <span
+                  typeof="button"
+                  onClick={(e) => changeStep(currentStep - 1, e)}
+                >
+                  Voltar
+                </span>
+              </button>
+
+              {!isLastStep ? (
+                <button type="submit">
+                  <span>Avançar</span>
+                  <GrFormNext />
+                </button>
+              ) : (
+                <button type="button">
+                  <span>Enviar</span>
+                  <FiSend />
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
